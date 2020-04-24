a12f74ee89b2edc6a716d123477f5338e2efddfe
fatimaromagnoli <fatinhadorei@hotmail.com>
2020-04-23 20:45:42 -0700

    delete

---
+++ routes/api.js
@@ -0,0 +1,52 @@
+// LOAD DATA
+// Requiring json data file from the db folder 
+
+const path = require('path');
+const db = require("../db/db.json");
+const fs = require('fs');
+
+let jsonData = db;
+
+// Routing
+module.exports = (app) => {
+
+  // GET db data
+  app.get("/api/notes", (req, res) => {
+    res.json(jsonData);
+  });
+
+  // POST new note
+  app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
+
+    if (jsonData.length === 0) {
+      newNote.id = 1;
+    }
+    else {
+      let newID = jsonData[jsonData.length - 1].id + 1;
+      newNote.id = newID;
+    }
+
+    jsonData.push(newNote);
+
+    fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(jsonData), err => {
+      if (err) throw err;
+      console.log('New note added successfully');
+    });
+
+    res.json(newNote);
+  });
+
+  app.delete("/api/notes/:id", (req, res) => {
+    let deleteID = req.params.id;
+
+    jsonData = jsonData.filter(obj => obj.id != deleteID);
+
+    fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(jsonData), err => {
+      if (err) throw err;
+      console.log('Note deleted successfully');
+    });
+
+    res.send('DELETE request to homepage');
+  });
+}
